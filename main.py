c1b31ab3fa546879efa186ca6eee7776f3480336
Borfeev <sundaycather@gmail.com>
2023-03-20 23:22:20 +0300

    init

---
+++ main.py
@@ -0,0 +1,69 @@
+import file_operations
+import random
+from faker import Faker
+
+letters_mapping = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+skills_list = ["Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
+               "Стремительный удар", "Кислотный взгляд", "Тайный побег",
+               "Ледяной выстрел", "Огненный заряд"
+               ]
+random_skills_list = random.sample(skills_list, 3)
+fake = Faker("ru_RU")
+
+print("check git")
+
+def main():
+    for number in range(10):
+        runic_skills = []
+        for one_skill in random_skills_list:
+            for symbol_1, symbol_translation in letters_mapping.items():
+                one_skill = one_skill.replace(symbol_1, symbol_translation)
+            runic_skills.append(one_skill)
+
+        context = {
+            "first_name": fake.first_name_male(),
+            "last_name": fake.last_name_male(),
+            "job": fake.job(),
+            "town": fake.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": runic_skills[0],
+            "skill_2": runic_skills[1],
+            "skill_3": runic_skills[2],
+        }
+
+        name_file = r"folder with characters\character {}.svg".format(number)
+        file_operations.render_template("character_template.svg",
+                                        name_file, context)
+
+    print("Проверка и подключение гита")
+
+if __name__ == "__main__":
+    main()
